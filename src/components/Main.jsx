d185c6e3ba1453d3397338657f31599a2abaa523
Matthew Pelnik <matthew.pelnik@icloud.com>
2023-03-07 09:39:56 -0600

    add Main and Navbar layout

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { Navbar } from '.';
 
 function Main() {
   return (
-    <Routes>
-      <Route path="/" element={<div>Test</div>} />
-    </Routes>
+    <div className="mainContainer" id="mainContainer">
+      <Navbar />
+      <Routes>
+        <Route path="*" element={<div className="mainContent">Test</div>} />
+      </Routes>
+      
+    </div>
+
   );
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <div className="navbar" id="navbar">
+      <div className="subheader" id="leftHeader">Left header</div>
+      <div className="subheader" id="rightHeader">Right Header</div>
+    </div>
+  );
+}
+
+export default Navbar;
