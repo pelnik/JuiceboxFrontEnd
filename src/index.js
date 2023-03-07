7e148c3024fb0578a0fab6cf1088255f0af44e9e
Cesar bacaro-ley <cesarbacaro-ley@Cesars-MacBook-Pro.local>
2023-03-07 09:12:46 -0600

    creating the Routes

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { ReactDOM } from "react-dom/client";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+const container = document.getElementById("app");
+const root = ReactDOM.createRoot(container);
+
+root.render(<Main />);
