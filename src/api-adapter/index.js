88796a4d03e03f3999cc9090064a27a58c4437af
Cesar bacaro-ley <cesarbacaro-ley@Cesars-MacBook-Pro.local>
2023-03-07 12:23:43 -0600

    posts

--- src/api-adapter/index.js
+++ src/api-adapter/index.js
@@ -1,30 +1,53 @@
 import { saveToLocalStorage } from "../utils/localStorage";
 
-const BASE_URL = 'https://juicebox-mld2.onrender.com/api';
+const BASE_URL = "https://juicebox-mld2.onrender.com/api";
 
 export async function login(username, password) {
-  console.log('login');
+  console.log("login");
 
   try {
     const response = await fetch(`${BASE_URL}/users/login`, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         username,
-        password
-      })
+        password,
+      }),
     });
     const fullResponse = await response.json();
     const token = fullResponse.token;
-    console.log('api token return', token)
+    console.log("api token return", token);
 
     if (token !== undefined) {
       saveToLocalStorage(token);
     }
 
     return token;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function getPosts(token) {
+  console.log("login");
+
+  try {
+    const response = await fetch(`${BASE_URL}/posts`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    const fullResponse = await response.json();
+
+    const posts = fullResponse.posts;
+    console.log("Posts!!!!!!!", posts);
+
+    return posts;
   } catch (error) {
     console.error(error);
   }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,12 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import { Navbar } from ".";
 import Login from "./Login";
-import {getToken} from '../utils/localStorage.js';
+import { getToken } from "../utils/localStorage.js";
+import Posts from "./Posts";
 
 function Main() {
-  const [token, setToken] = useState('');
+  const [token, setToken] = useState("");
 
   useEffect(() => {
     const localToken = getToken();
@@ -13,15 +14,17 @@ function Main() {
     if (localToken !== undefined) {
       setToken(localToken);
     }
-  }, [])
-
+  }, []);
 
   return (
     <div className="mainContainer" id="mainContainer">
       <Navbar />
       <Routes>
-        <Route path="/login" element={<Login setToken={setToken} token={token} />} />
-        <Route path="*" element={<div className="mainContent">{token}</div>} />
+        <Route
+          path="/login"
+          element={<Login setToken={setToken} token={token} />}
+        />
+        <Route path="*" element={<Posts />} />
       </Routes>
     </div>
   );
